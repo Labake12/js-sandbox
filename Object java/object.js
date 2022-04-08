d47e6f2ac891ff8e9694fd63cdda5c5b26cbf98e
Labake12 <101086212+Labake12@users.noreply.github.com>
2022-04-09 00:04:54 +0100

    Add files via upload

---
+++ Object java/object.js
@@ -0,0 +1,34 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+var user = {
+    username : "labake",
+    password : "lab123"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+var database = [
+    {
+        username : "labake",
+        password : "lab123"
+    }
+]
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+var newsFeed = [
+    {
+        username : "function",
+        timeline : "monday"
+    },
+    {
+        username : "array",
+        timeline : "tuesday"
+    },
+    {
+        username : "object",
+        timeline : "wednesday"
+    }
+]
